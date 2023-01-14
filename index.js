7f9d851e2715e2902206eb864f40ca611a289df8
Brett de Bear <bkdebear@gmail.com>
2023-01-14 15:22:21 -0800

    lab complete yay

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', (e) => {
+    let newContent = document.querySelector('#text');
+    text.textContent = 'This is really cool!'
+    
+})
